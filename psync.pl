d30b776a7f1afdfe77611c681c43b97f83d36315
Markus Lindberg <markus.lindberg89@gmail.com>
2021-10-15 13:18:30 +0200

    Include counting of duplicate files

--- psync.pl
+++ psync.pl
@@ -9,38 +9,39 @@ use File::Path "make_path";
 use File::Basename;
 use File::Find::Rule;
 use File::Copy;
+use Set::Scalar;
 
 # GLOBALS
 my $MD5 = Digest::MD5->new;
-my $ET  = Image::ExifTool->new;
-
-my %PARAMS = (
-  VERBOSE => 0,
-  DRY_RUN => 0  
-);
-
+my $ET  = Image::ExifTool->new; $ET->Options(FastScan => 2);
+my %PARAMS = ( VERBOSE => 1, DRY_RUN => 0  );
 my $usage = << "EOF";
 usage:
-  psync.pl [-h] [-v] [-d] SOURCE DESTINATION
+  psync.pl [-h, -q, -d, --mv] SOURCE DESTINATION
 
   Optional arguments:
     -h, --help     Show this message and exit
-    -v, --verbose  Increase terminal output
-    -d, --dry-run  Don't actually copy
+    -q, --quiet    Don't print information to terminal
+    -d, --dry-run  Don't actually copy/move files, just print
+                   (unless --quiet is set)
+    --mv           Move instead of copy files from SOURCE
+                   to DESTINATION
 
   Positional arguments:
     SOURCE       Directory to search for images and videos
-    TARGET       Directory to where files will be copied
+    TARGET       Directory to where files will be copied to
 
 Psync obtain timestamps from file metadata and copies files to
 the destination within folders and with names corresponding to
 the timestamps of the files. Files are hashed to ensure that 
 only unique files are copied.
+
 EOF
 
-
 {
-  package ImageFile;
+  # A class to store image and video files and acces timestamps
+  # for their creation date and md5sum
+  package MediaFile;
   use Moose;
 
   has filename  => ( isa => 'Str', is  => 'rw');
@@ -67,16 +68,30 @@ EOF
   }
 }
 
+main();
+
 sub main {
   my ($source_dir, $dest_dir) = parse_argv(\%PARAMS);
 
+  my %cp = (
+    cmd     => \&copy,
+    cmd_str => "cp",
+    str     => "copy"
+  );
+
+  if ($PARAMS{MOVE}) {
+    $cp{cmd}     = \&move;
+    $cp{cmd_str} = "mv";
+    $cp{str}     = "move";
+  }
+  
   # In order to avoid checks against VERBOSE in each for-loop iteration,
-  # $put is used. If verbosity is turned off, it does nothing, otherwise
-  # it prints the passed string
+  # $put is used instead of `print`. If verbosity is turned off,
+  # it does nothing, otherwise it prints the passed string
   my $put;
   if ($PARAMS{VERBOSE}) { $put = sub { print shift }; } else { $put = sub { return 0 }; }
 
-  my $img_files = scan_dir($source_dir);
+  my ($img_files, $duplicates) = scan_dir($source_dir);
   for my $img (@{$img_files}) {
     my @dest  = suggest_destination($img);
     my $dfile = "$dest[1]$dest[2]";    # New timestamped filename plus extension
@@ -93,26 +108,27 @@ sub main {
       if (digest_file($dpath) eq $img->digest) {
         $file_already_exist = 1;
       } else {
-        $dfile = "$dest[1].($i)$dest[2]";
+        $dfile = "$dest[1]($i)$dest[2]";
         $ddir  = "$dest_dir/$dest[0]";     # Destination dir plus timestamped subdirs
         $dpath = "$ddir/$dfile";           # Destination filepath
       }
     }
-    my $fn = $img->filename;
+
     if ($file_already_exist) {
-      $put->("$fn already exist at: $dpath\n" );
+      $put->("$img->{filename} already exist at: $dpath\n" );
       next;
     }
 
-    $put->("cp $fn $dpath\n");
+    $put->("$cp{cmd_str} $img->{filename} $dpath\n");
     unless ($PARAMS{DRY_RUN}) {
       unless (-d $ddir) {
         $put->("mkdir $ddir");
         make_path($ddir);
       }
-      copy($fn, $dpath) or print STDERR "Could not copy $fn to $dpath\n";
+      $cp{cmd}->($img->{filename}, $dpath) or print STDERR "Could not $cp{str} $img->{filename} to $dpath\n";
     }
   }
+  $put->("Nr of duplicate files found at source dir: " . scalar @{$duplicates} . "\n");
 }
 
 sub parse_argv {
@@ -124,8 +140,8 @@ sub parse_argv {
       if ($ARGV[$i] eq '-h' || $ARGV[$i] eq '--help') {
         die $usage 
       }
-      if ($ARGV[$i] eq '-v' || $ARGV[$i] eq '--verbose') {
-        $params->{VERBOSE} = 1;
+      if ($ARGV[$i] eq '-q' || $ARGV[$i] eq '--quiet') {
+        $params->{VERBOSE} = 0;
         next;
       }
       if ($ARGV[$i] eq '-d' || $ARGV[$i] eq '--dry-run') {
@@ -151,8 +167,7 @@ sub parse_argv {
 }
 
 sub suggest_destination {
-  my $img    = shift; # Ref to element outputted from `scan_dir`
-
+  my $img     = shift;
   my $oldname = $img->filename;
   my ($fext)  = $oldname =~ /(\.[^.]+)$/;
   my $ts      = parse_date($img->timestamp);
@@ -163,20 +178,34 @@ sub suggest_destination {
 
 sub scan_dir {
   # Takes a directory and search for image and video files
-  # Returns an array of ImageFiles 
+  # Returns an array of ImageFiles to be copied/moved and
+  # an array of duplicate files that shouldn't be moved/copied.
   my $dir = shift;
+  my $seen_md5sums = Set::Scalar->new();
   my @retval;
+  my @duplicates;
+
+  # Right now, duplicate files are simply collected, whith no notion
+  # of what file(s) are identical to the duplicate...
+
   # Scan for jpg, png, jpeg and mp4 files
   for my $imf (File::Find::Rule->file()
                                ->name( '*\.[jJ][pP][gG]',     '*\.[pP][nN][gG]',
                                        '*\.[jJ][pP][eE][gG]', '*\.[mM][pP]4')
                                ->in ($dir)) {
 
-     my $image = ImageFile->new( filename => $imf);
-     $image->digest_file(); $image->get_timestamp();
-     push @retval, $image;
+     my $image = MediaFile->new( filename => $imf);
+     $image->digest_file();
+    
+     if ( $seen_md5sums->has($image->digest)) {
+       push @duplicates, $image;
+     } else {
+       $seen_md5sums->insert($image->digest);
+       $image->get_timestamp();
+       push @retval, $image;
+     }
   }
-  return \@retval;
+  return (\@retval, \@duplicates);
 }
 
 sub parse_date {
@@ -189,13 +218,12 @@ sub parse_date {
 }
 
 sub digest_file {
-  my $fname = shift;
-  open my $fh, '<:raw', $fname or die "Cannot open file: $fname\n";
+  my $filename = shift;
+  open my $fh, '<:raw', $filename or die "Cannot open file: $filename, $!\n";
   $MD5->addfile($fh);
   my $digest = $MD5->digest;
   $MD5->reset();
   close $fh;
-  return $digest;
+  return $digest
 }
 
-main()
